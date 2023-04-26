a73076ae4d4f076596ab18527b9fc9dff92616e6
yettera765 <88224234+yettera765@users.noreply.github.com>
2023-04-26 17:09:02 +0800

    Update install.sh

--- sing/install.sh
+++ sing/install.sh
@@ -2,9 +2,12 @@
 
 set -e -o pipefail
 
-if [ -d /usr/local/go ]; then
-  export PATH="$PATH:/usr/local/go/bin"
-fi
+go_version=$(curl -s https://raw.githubusercontent.com/actions/go-versions/main/versions-manifest.json | grep -oE '"version": "[0-9]{1}.[0-9]{1,}(.[0-9]{1,})?"' | head -1 | cut -d':' -f2 | sed 's/ //g; s/"//g')
+curl -Lo go.tar.gz "https://go.dev/dl/go$go_version.linux-amd64.tar.gz"
+sudo rm -rf /usr/local/go
+sudo tar -C /usr/local -xzf go.tar.gz
+rm go.tar.gz
+export PATH="$PATH:/usr/local/go/bin"
 
 HEAD=$(git ls-remote https://github.com/SagerNet/sing-box.git HEAD | awk '{ print $1 }')
 COMMIT=${HEAD:0:7}
